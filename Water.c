9d816927d0229e62175f3ef9e86d943b7ef201a2
khayne31 <42650648+khayne31@users.noreply.github.com>
2018-10-01 13:34:07 -0400

    Delete Water.c

--- Water.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void){
-    printf("Give me water mins bish: ");
-   int minutes= get_int();
-   printf("Minutes: %i \n Water: %i \n", minutes, minutes*12);
-}
